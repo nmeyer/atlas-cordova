b901164fc5d8f86d6b347f4eb728d80716f2ea97
Nick Meyer <nick@look-see.com>
2015-04-16 14:41:24 -0700

    Fill in ConversationListView protocol methods with dummy data

---
+++ src/ios/AtlasCordova.m
@@ -0,0 +1,15 @@
+#import "ConversationsListViewController.h"
+
+@implementation AtlasCordova
+
++ (void)showConversations:(CDVInvokedUrlCommand *)command
+{
+
+}
+
++ (void)hideConversations:(CDVInvokedUrlCommand *)command
+{
+
+}
+
+@end
--- src/ios/ConversationsListViewController.m
+++ src/ios/ConversationsListViewController.m
@@ -6,13 +6,13 @@
 #pragma mark - ATLConversationListViewControllerDelegate
 - (void)conversationListViewController:(ATLConversationListViewController *)conversationListViewController didSelectConversation:(LYRConversation *)conversation
 {
-    
+    NSLog(@"didSelectConversation");
 }
 
 #pragma mark - ATLConversationListViewControllerDataSource
 - (NSString *)conversationListViewController:(ATLConversationListViewController *)conversationListViewController titleForConversation:(LYRConversation *)conversation
 {
-    
+    return conversation.description;
 }
 
 @end
